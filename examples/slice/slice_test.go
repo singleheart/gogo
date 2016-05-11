110ada6eff4ed3eb4f8f079891d17d36ad3b9af7
singleheart <singleheart@gmail.com>
2016-05-12 00:51:18 +0900

    add slice capacity and copy tests

--- examples/slice/slice_test.go
+++ examples/slice/slice_test.go
@@ -32,3 +32,61 @@ func Example_append() {
 	// [사과 바나나 토마토 포도 딸기]
 	// [사과 바나나 포도 딸기]
 }
+
+func Example_sliceCap() {
+	nums := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(nums)
+	fmt.Println("len:", len(nums))
+	fmt.Println("cap:", cap(nums))
+	fmt.Println()
+
+	sliced1 := nums[:3]
+	fmt.Println(sliced1)
+	fmt.Println("len:", len(sliced1))
+	fmt.Println("cap:", cap(sliced1))
+	fmt.Println()
+
+	sliced2 := nums[2:]
+	fmt.Println(sliced2)
+	fmt.Println("len:", len(sliced2))
+	fmt.Println("cap:", cap(sliced2))
+	fmt.Println()
+
+	sliced3 := sliced1[:4]
+	fmt.Println(sliced3)
+	fmt.Println("len:", len(sliced3))
+	fmt.Println("cap:", cap(sliced3))
+	fmt.Println()
+
+	nums[2] = 100
+	fmt.Println(nums, sliced1, sliced2, sliced3)
+	// Output:
+	// [1 2 3 4 5]
+	// len: 5
+	// cap: 5
+	//
+	// [1 2 3]
+	// len: 3
+	// cap: 5
+	//
+	// [3 4 5]
+	// len: 3
+	// cap: 3
+	//
+	// [1 2 3 4]
+	// len: 4
+	// cap: 5
+	//
+	// [1 2 100 4 5] [1 2 100] [100 4 5] [1 2 100 4]
+}
+
+func Example_sliceCopy() {
+	src := []int{30, 20, 50, 10, 40}
+	dest := make([]int, len(src))
+	for i := range src {
+		dest[i] = src[i]
+	}
+	fmt.Println(dest)
+	// Output: [30 20 50 10 40]
+}
